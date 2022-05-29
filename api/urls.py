b7f2c84fda24ec5013e0e124c0a4792dbc91e5bf
RyotaNishio <78110301+Sena-7v@users.noreply.github.com>
2022-05-29 10:14:55 +0900

    [Finish]Api

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import CreateUserView, PostListView, PostRetrieveView, TaskListView, TaskRetrieveView, TaskViewSet
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
+
+urlpatterns = [
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('list-post/', PostListView.as_view(), name='list-post'),
+    path('detail-post/<str:pk>/', PostRetrieveView.as_view(), name='detail-post'),
+    path('list-task/', TaskListView.as_view(), name='list-post'),
+    path('detail-task/<str:pk>/', TaskRetrieveView.as_view(), name='detail-post'),
+    path('auth/', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
+]
